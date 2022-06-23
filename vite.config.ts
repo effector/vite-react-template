64b54afb9889bb3bf452325c6578d061b4dde4da
sergeysova <mail@sergeysova.com>
2022-06-23 17:59:02 +0400

    6 files modified

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import babel from '@rollup/plugin-babel';
+import { babel } from '@rollup/plugin-babel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    babel({ extensions: ['.ts'], babelHelpers: 'bundled' }) as any,
-    react(),
-  ],
+  plugins: [babel({ extensions: ['.ts'], babelHelpers: 'bundled' }), react()],
 });
